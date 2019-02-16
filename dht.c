6bba6e1dddfb99a04cfb57eb00ee8a9886f5256c
Ashish Kumar <ashish.jspr@gmail.com>
2019-02-16 06:38:21 +0530

    Update dht.c

--- dht.c
+++ dht.c
@@ -1,12 +1,5 @@
-/*
-DHT Library 0x03
 
-copyright (c) Davide Gironi, 2012
-
-Released under GPLv3.
-Please refer to LICENSE file for licensing information.
-*/
-
+//DHT 
 
 #include <stdio.h>
 #include <string.h>
