04b0d2d3634146833ab1dd44bb994b25fdc79f0a
Ashish Kumar <ashish.jspr@gmail.com>
2019-02-16 06:39:19 +0530

    Update dht.h

--- dht.h
+++ dht.h
@@ -1,14 +1,5 @@
-/*
-DHT Library 0x03
 
-copyright (c) Davide Gironi, 2012
-
-Released under GPLv3.
-Please refer to LICENSE file for licensing information.
-
-References:
-  - DHT-11 Library, by Charalampos Andrianakis on 18/12/11
-*/
+//DHT Header
 
 
 #ifndef DHT_H_
